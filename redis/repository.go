4dafde9b53e3527c27af00906e35596a6d56f68a
Rian-rgb <rian3903@gmail.com>
2026-06-13 15:20:31 +0700

    adjustment func redis repository

--- redis/repository.go
+++ redis/repository.go
@@ -6,7 +6,6 @@ import (
 )
 
 type Repository interface {
-	Get(ctx context.Context, key string) (string, error)
 	Exists(ctx context.Context, key string) (bool, error)
 	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
 	Delete(ctx context.Context, key string) error
