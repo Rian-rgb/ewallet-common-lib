cdb53a6b5715ab93379f1de191e04a1dffc96c2e
Rian-rgb <rian3903@gmail.com>
2026-06-14 23:37:06 +0700

    Refactor response message code and add validator.go

--- response/message.go
+++ response/message.go
@@ -2,9 +2,10 @@ package response
 
 const (
 	SuccessMessage             = "Success"
-	BadRequestMessage          = "Invalid request"
 	UnauthorizedMessage        = "Authorization token is required"
 	InvalidTokenMessage        = "Invalid token"
 	TokenExpiredMessage        = "Your token has expired. Please login again."
 	InternalServerErrorMessage = "An unexpected error occurred. Please try again later."
+	InvalidRequest             = "Invalid request"
+	InvalidJSONFormat          = "Invalid JSON format"
 )
---
+++ response/validator.go
@@ -0,0 +1,53 @@
+package response
+
+import (
+	"errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
+
+func MapValidationErrors(err error) []ValidationErrorField {
+	if err == nil {
+		return nil
+	}
+
+	var validationErrors validator.ValidationErrors
+	ok := errors.As(err, &validationErrors)
+	if !ok {
+		return nil
+	}
+
+	var listErrors []ValidationErrorField
+
+	for _, f := range validationErrors {
+		var msg string
+
+		// f.Field() defaultnya mengambil nama field struct (misal: "Username")
+		// Kita ubah ke huruf kecil agar cocok dengan format JSON key umum
+		fieldName := strings.ToLower(f.Field())
+
+		// Atur pesan error kustom bahasa Indonesia berdasarkan Tag validatornya
+		switch f.Tag() {
+		case "required":
+			msg = fmt.Sprintf("Field %s wajib diisi", fieldName)
+		case "email":
+			msg = fmt.Sprintf("Format %s tidak valid", fieldName)
+		case "min":
+			msg = fmt.Sprintf("Field %s minimal berisi %s karakter", fieldName, f.Param())
+		case "max":
+			msg = fmt.Sprintf("Field %s maksimal berisi %s karakter", fieldName, f.Param())
+		case "numeric":
+			msg = fmt.Sprintf("Field %s harus berupa angka", fieldName)
+		default:
+			msg = fmt.Sprintf("Field %s tidak memenuhi aturan %s", fieldName, f.Tag())
+		}
+
+		listErrors = append(listErrors, ValidationErrorField{
+			Field:   fieldName,
+			Message: msg,
+		})
+	}
+
+	return listErrors
+}
