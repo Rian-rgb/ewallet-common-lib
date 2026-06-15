20022caab543a36aa0a48df4b88cfb1dce7ff747
Rian-rgb <rian3903@gmail.com>
2026-06-15 22:25:30 +0700

    updates 2

--- response/message.go
+++ response/message.go
@@ -1,10 +1,11 @@
 package response
 
 const (
-	SuccessMessage           = "success"
-	UnauthorizedMessage      = "authorization token is required"
-	InvalidTokenMessage      = "invalid token"
-	TokenExpiredMessage      = "your token has expired. Please login again."
-	InvalidRequestMessage    = "invalid request"
-	InvalidJSONFormatMessage = "invalid JSON format"
+	SuccessMessage             = "success"
+	UnauthorizedMessage        = "authorization token is required"
+	InvalidTokenMessage        = "invalid token"
+	TokenExpiredMessage        = "your token has expired. Please login again"
+	InternalServerErrorMessage = "an unexpected error occurred. Please try again later"
+	InvalidRequestMessage      = "invalid request"
+	InvalidJSONFormatMessage   = "invalid JSON format"
 )
