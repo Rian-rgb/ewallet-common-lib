8318cc8dfdc7dce2432c18a791568881dd64fd43
Rian-rgb <rian3903@gmail.com>
2026-06-16 08:55:58 +0700

    feat(redis): add const UserTokenDuration

--- redis/expired_duration.go
+++ redis/expired_duration.go
@@ -5,5 +5,6 @@ import "time"
 type ExpiredDuration time.Duration
 
 const (
+	UserTokenDuration    ExpiredDuration = ExpiredDuration(5 * time.Minute)
 	RefreshTokenDuration ExpiredDuration = ExpiredDuration(72 * time.Hour)
 )
