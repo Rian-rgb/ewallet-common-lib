a5acb733ca3220c5e9fa8ae653f02d512ed4609f
Rian-rgb <rian3903@gmail.com>
2026-06-19 07:42:34 +0700

    refactor(response): validator.go changes validate message from indonesian become english

--- response/validator.go
+++ response/validator.go
@@ -26,29 +26,48 @@ func MapValidationErrors(err error) []ValidationErrorField {
 
 		switch f.Tag() {
 		case "required":
-			msg = fmt.Sprintf("Field %s wajib diisi", fieldName)
+			msg = fmt.Sprintf("The %s field is required", fieldName)
+
 		case "email":
-			msg = fmt.Sprintf("Format %s tidak valid", fieldName)
+			msg = fmt.Sprintf("The %s field must be a valid email address", fieldName)
+
 		case "min":
-			msg = fmt.Sprintf("Field %s minimal berisi %s karakter", fieldName, f.Param())
+			msg = fmt.Sprintf(
+				"The %s field must be at least %s characters long",
+				fieldName,
+				f.Param(),
+			)
+
 		case "max":
-			msg = fmt.Sprintf("Field %s maksimal berisi %s karakter", fieldName, f.Param())
+			msg = fmt.Sprintf(
+				"The %s field must not exceed %s characters",
+				fieldName,
+				f.Param(),
+			)
+
 		case "numeric":
-			msg = fmt.Sprintf("Field %s harus berupa angka", fieldName)
+			msg = fmt.Sprintf("The %s field must be numeric", fieldName)
+
 		case "oneof":
 			msg = fmt.Sprintf(
-				"Field %s harus bernilai salah satu dari: %s",
+				"The %s field must be one of: %s",
 				fieldName,
 				f.Param(),
 			)
+
 		case "gt":
 			msg = fmt.Sprintf(
-				"Field %s harus lebih besar dari %s",
+				"The %s field must be greater than %s",
 				fieldName,
 				f.Param(),
 			)
+
 		default:
-			msg = fmt.Sprintf("Field %s tidak memenuhi aturan %s", fieldName, f.Tag())
+			msg = fmt.Sprintf(
+				"The %s field failed validation for rule '%s'",
+				fieldName,
+				f.Tag(),
+			)
 		}
 
 		listErrors = append(listErrors, ValidationErrorField{
